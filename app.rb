5d36fbac58662ce8471baed78a3a2864f5fca51c
susietruitt <91689219+susietruitt@users.noreply.github.com>
2024-04-07 21:46:12 +0000

    firstcommit

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+<h1>Welcome to Rock-Paper-Scissors!</h1>
+
+<p>From <a href=\"https://en.wikipedia.org/wiki/Rock_paper_scissors\">Wikipedia</a>:</p>
+<br>
+<p>Rock-paper-scissors (also known as paper, scissors, stone or other variants) is a hand game usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand.</p>
+"
+
 end
